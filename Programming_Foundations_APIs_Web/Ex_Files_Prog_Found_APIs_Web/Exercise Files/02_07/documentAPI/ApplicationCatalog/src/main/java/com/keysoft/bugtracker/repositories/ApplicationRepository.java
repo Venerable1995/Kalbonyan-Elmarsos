a2e86999601256dbcde427b9f40e4fc58d9a52e4
Mohamed Saeed <88166334+Venerable1995@users.noreply.github.com>
2021-12-13 16:50:30 +0200

    Delete Programming_Foundations_APIs_Web/Ex_Files_Prog_Found_APIs_Web/Exercise Files directory

--- Programming_Foundations_APIs_Web/Ex_Files_Prog_Found_APIs_Web/Exercise Files/02_07/documentAPI/ApplicationCatalog/src/main/java/com/keysoft/bugtracker/repositories/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.keysoft.bugtracker.repositories;
-
-import main.java.com.keysoft.bugtracker.domain.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Integer> {
-}
