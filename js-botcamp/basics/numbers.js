1a8245b4190a5b90bd810faa7e4e19271dfb3cf5
Mohamed Saeed <88166334+Venerable1995@users.noreply.github.com>
2021-12-13 17:03:38 +0200

    Delete js-botcamp directory

--- js-botcamp/basics/numbers.js
+++
@@ -1,8 +0,0 @@
-let age = 26
-let dogYears = (age + 1) / 7
- console.log(dogYears)
- // -------challenge ----------------------------
- let studentScore = 41
- let maxScore = 100
- let percent = studentScore/maxScore * 100
- console.log(percent)
--- js-botcamp/objects/const.js
+++
@@ -1,7 +0,0 @@
-const person = {
-    age:27
-}
-person.age=28
-//person = {}
-//isRaining =false
-console.log(person)
