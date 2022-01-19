2a0c7601fd5a9c202c4699167158271130c9caaa
Mohamed Saeed <childishperm@gmail.com>
2022-01-19 13:38:28 +0200

    remove dummy expense data

--- Udemy/React-Course-Project/expensify-app/src/app.js
+++ Udemy/React-Course-Project/expensify-app/src/app.js
@@ -14,13 +14,6 @@ import'react-dates/lib/css/_datepicker.css';
 const store = configureStore()
 
 
-
-store.dispatch(addExpenses({description:'water bill',amount:100,createdAt:3000}))
-store.dispatch(addExpenses({description:'gas bill',amount:200,createdAt:2000}))
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses , state.filters);
-console.log(visibleExpenses)
-
 const jsx=(
     <Provider store={store}>
     <AppRouter />
